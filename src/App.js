c60b07cd9a45132fa2e1e4da4283da0fdebef099
logicByHari <harsh@logicrays.com>
2024-05-27 15:33:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar title="Harsh PWA"/>
+    </div>
+  );
+}
+
+export default App;
