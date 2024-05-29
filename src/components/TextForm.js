99cda7a7ea973aa60a0aeab1259dcea68e2de5be
logicByHari <harsh@logicrays.com>
2024-05-29 19:16:17 +0530

    update data in single component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,13 +8,20 @@ export default function TextForm(props) {
   const handelUpChange = (event) => {
     setText(event.target.value);
     }
-    const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("Enter text here");
     
-    return (
-      <div className="mb-3">
-        <h3>{props.heading}</h3>
-        <textarea className="form-control" id="myBox" rows="3" value={text} onChange={handelUpChange}></textarea>
-        <button className="btn btn-primary" onClick={handelUpClick}>Convert To Upper</button>
+  return (
+      <div>
+        <div className="container mb-3">
+          <h3>{props.heading}</h3>
+          <textarea className="form-control" id="myBox" rows="3" value={text} onChange={handelUpChange}></textarea>
+          <button className="btn btn-primary" onClick={handelUpClick}>Convert To Upper</button>
+        </div>
+        <div className="container">
+        <h1>Summary</h1>
+        <p>{text.split(" ").length}words and { text.length}</p>
+        </div>
+        
       </div>
     )
 }
